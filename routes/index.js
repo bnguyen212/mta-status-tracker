6df30ce8958426dff0344f3dd09acc46cb9da9af
Brian <ptnguyen212@gmail.com>
2020-08-24 09:45:05 -0700

    parse timestamp in local time

--- routes/index.js
+++ routes/index.js
@@ -21,11 +21,11 @@ router.get('/status', function (req, res, next) {
 					const response = {
 						route: record.routeName,
 						delayed: record.delay,
-						timestamp: record.datetime
+						timestamp: moment(record.datetime).format()
 					};
 					res.json(response);
 				} else {
-					res.json({ error: `No information is available for route ${req.query.route}.` });
+					res.json({ message: `No information is available for route ${req.query.route}.` });
 				}
 			})
 			.catch(err => {
@@ -42,36 +42,41 @@ router.get('/uptime', function (req, res, next) {
 		Record.findAll({
 			where: { routeName: req.query.route },
 			order: [['id', 'ASC']]
-		}).then(records => {
-			if (!records.length) {
-				return res.json({ message: `No record found for route ${req.query.route}.` });
-			}
-			let totalTime = 0;
-			let downTime = 0;
-			for (let i = 1; i < records.length; i++) {
-				const difference = records[i].datetime - records[i - 1].datetime;
-				if (!records[i].delay) {
-					downTime += difference;
+		})
+			.then(records => {
+				if (!records.length) {
+					return res.json({ message: `No record found for route ${req.query.route}.` });
 				}
-				totalTime += difference;
-			}
-			// calculate the time between now and last record
-			const now = new Date();
-			const fromNowtoLastRecord = now - records[records.length - 1].datetime;
-			totalTime += fromNowtoLastRecord;
-			if (records[records.length - 1].delay) {
-				downTime += fromNowtoLastRecord;
-			}
+				let totalTime = 0;
+				let downTime = 0;
+				for (let i = 1; i < records.length; i++) {
+					const difference = records[i].datetime - records[i - 1].datetime;
+					if (!records[i].delay) {
+						downTime += difference;
+					}
+					totalTime += difference;
+				}
+				// calculate the time between now and last record
+				const now = new Date();
+				const fromNowtoLastRecord = now - records[records.length - 1].datetime;
+				totalTime += fromNowtoLastRecord;
+				if (records[records.length - 1].delay) {
+					downTime += fromNowtoLastRecord;
+				}
 
-			const response = {
-				route: req.query.route,
-				uptime: 1 - downTime / totalTime,
-				start: records[0].datetime,
-				end: now
-			};
+				const response = {
+					route: req.query.route,
+					uptime: 1 - downTime / totalTime,
+					start: records[0].datetime,
+					end: now
+				};
 
-			res.json(response);
-		});
+				res.json(response);
+			})
+			.catch(err => {
+				console.error(err);
+				res.json({ error: 'Internal error, please try again later.' });
+			});
 	} else {
 		res.json({ error: 'Please provide a valid route' });
 	}
